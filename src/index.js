9740d76cc22d218b70eabd3d5114979da8027b20
seiya kawamura <seiya.kawamura@gmail.com>
2016-12-29 12:30:18 +0900

    一旦コミット

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './scripts/App';
+import './styles/index.css';
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+);
