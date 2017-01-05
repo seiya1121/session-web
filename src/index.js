761cd79e304f9bf2d68ffbdf91fbc7a4ba81b704
seiya kawamura <seiya.kawamura@gmail.com>
2017-01-06 01:35:47 +0900

    諸々修正 for heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './scripts/components/app';
+import App from './scripts/components/app.jsx';
 import './styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
