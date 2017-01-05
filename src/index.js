7b7b3705a97bbc8212433e04dca7b0da727af1da
seiya kawamura <seiya.kawamura@gmail.com>
2017-01-06 01:49:34 +0900

    ファイル名変更

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './scripts/components/app.jsx';
+import App from './scripts/components/app';
 import './styles/index.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
