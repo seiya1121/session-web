0d41553c4d571ea692ca9e78bbcebbffd5a1eb95
seiya <seiya.kawamura@gmail.com>
2017-01-06 22:10:24 +0900

    諸々修正

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -1,15 +1,17 @@
-import autoprefixer from 'autoprefixer';
-import webpack from 'webpack';
-import HtmlWebpackPlugin from 'html-webpack-plugin';
-import ExtractTextPlugin from 'extract-text-webpack-plugin';
-import ManifestPlugin from 'webpack-manifest-plugin';
-import InterpolateHtmlPlugin from 'react-dev-utils/InterpolateHtmlPlugin';
-import url from 'url';
-import { paths } from './paths';
-import { getClientEnvironment } from './env';
+var autoprefixer = require('autoprefixer');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+var ManifestPlugin = require('webpack-manifest-plugin');
+var InterpolateHtmlPlugin = require('react-dev-utils/InterpolateHtmlPlugin');
+var url = require('url');
+var paths = require('./paths');
+var getClientEnvironment = require('./env');
 
-const ensureSlash = (path, needsSlash) => {
-  const hasSlash = path.endsWith('/');
+
+
+function ensureSlash(path, needsSlash) {
+  var hasSlash = path.endsWith('/');
   if (hasSlash && !needsSlash) {
     return path.substr(path, path.length - 1);
   } else if (!hasSlash && needsSlash) {
@@ -24,17 +26,17 @@ const ensureSlash = (path, needsSlash) => {
 // single-page apps that may serve index.html for nested URLs like /todos/42.
 // We can't use a relative path in HTML because we don't want to load something
 // like /todos/42/static/js/bundle.7289d.js. We have to know the root.
-const homepagePath = paths.appPackageJson.homepage;
-const homepagePathname = homepagePath ? url.parse(homepagePath).pathname : '/';
+var homepagePath = require(paths.appPackageJson).homepage;
+var homepagePathname = homepagePath ? url.parse(homepagePath).pathname : '/';
 // Webpack uses `publicPath` to determine where the app is being served from.
 // It requires a trailing slash, or the file assets will get an incorrect path.
-const publicPath = ensureSlash(homepagePathname, true);
+var publicPath = ensureSlash(homepagePathname, true);
 // `publicUrl` is just like `publicPath`, but we will provide it to our app
 // as %PUBLIC_URL% in `index.html` and `process.env.PUBLIC_URL` in JavaScript.
 // Omit trailing slash as %PUBLIC_PATH%/xyz looks better than %PUBLIC_PATH%xyz.
-const publicUrl = ensureSlash(homepagePathname, false);
+var publicUrl = ensureSlash(homepagePathname, false);
 // Get environment variables to inject into our app.
-const env = getClientEnvironment(publicUrl);
+var env = getClientEnvironment(publicUrl);
 
 // Assert this just to be safe.
 // Development builds of React are slow and not intended for production.
@@ -42,17 +44,29 @@ if (env['process.env'].NODE_ENV !== '"production"') {
   throw new Error('Production builds must have NODE_ENV=production.');
 }
 
+// This is the production configuration.
+// It compiles slowly and is focused on producing a fast and minimal bundle.
+// The development configuration is different and lives in a separate file.
 module.exports = {
+  // Don't attempt to continue if there are any errors.
   bail: true,
+  // We generate sourcemaps in production. This is slow but gives good results.
+  // You can exclude the *.map files from the build during deployment.
   devtool: 'source-map',
+  // In production, we only want to load the polyfills and the app code.
   entry: [
     require.resolve('./polyfills'),
     paths.appIndexJs
   ],
   output: {
+    // The build folder.
     path: paths.appBuild,
+    // Generated JS file names (with nested folders).
+    // There will be one main bundle, and one file per asynchronous chunk.
+    // We don't currently advertise code splitting but Webpack supports it.
     filename: 'static/js/[name].[chunkhash:8].js',
     chunkFilename: 'static/js/[name].[chunkhash:8].chunk.js',
+    // We inferred the "public path" (such as / or /my-project) from homepage.
     publicPath: publicPath
   },
   resolve: {
