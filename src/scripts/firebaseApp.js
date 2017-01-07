0339427fc8ec96450fa0e89b01d8e976ff10f10c
seiya kawamura <seiya.kawamura@gmail.com>
2017-01-08 03:36:44 +0900

    env判定

--- src/scripts/firebaseApp.js
+++ src/scripts/firebaseApp.js
@@ -2,8 +2,8 @@ import firebase from 'firebase';
 import { FIREBASE_CONFIG } from './api_key.js';
 import Rebase from 're-base';
 
+const ref = (process.env.NODE_ENV === 'production') ? 'session' : 'session-seiya'
 export const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
 export const firebaseDb = firebaseApp.database();
 export const firebaseAuth = firebaseApp.auth();
-// export const base = Rebase.createClass(FIREBASE_CONFIG, 'session');
-export const base = Rebase.createClass(FIREBASE_CONFIG, 'session-seiya');
+export const base = Rebase.createClass(FIREBASE_CONFIG, ref);
