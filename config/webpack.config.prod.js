65448c73d63e50c91ffc747cb05fb628d6c87a37
seiya kawamura <seiya.kawamura@gmail.com>
2017-01-14 23:42:59 +0900

    webpack修正

--- config/webpack.config.prod.js
+++ config/webpack.config.prod.js
@@ -149,6 +149,7 @@ module.exports = {
         loader: ExtractTextPlugin.extract('style', 'css?importLoaders=1!postcss')
         // Note: this won't work without `new ExtractTextPlugin()` in `plugins`.
       },
+      { test: /\.scss$/, loaders: ExtractTextPlugin.extract("style", "css", "sass") },
       // JSON is not enabled by default in Webpack but both Node and Browserify
       // allow it implicitly so we also enable it.
       {
