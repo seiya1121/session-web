7adda4cd087653aae841fc0510b2528ba1d80cf3
seiya kawamura <seiya.kawamura@gmail.com>
2017-01-14 23:18:20 +0900

    fix gitignore

---
+++ src/config/apiKey.js
@@ -0,0 +1,7 @@
+export const FIREBASE_CONFIG = {
+  apiKey: process.env.REACT_APP_FIREBASE_DEV_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_DEV_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DEV_DATABASE_URL,
+  storageBucket: process.env.REACT_APP_FIREBASE_DEV_STORAGE_BUCKET,
+};
+export const YOUTUBE_API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
