2e9b75054389009f01bda8be5cbd624da8ba466e
seiya kawamura <seiya.kawamura@gmail.com>
2017-01-28 16:26:32 +0900

    不要なconsole.logを削除

--- src/config/firebaseApp.js
+++ src/config/firebaseApp.js
@@ -3,7 +3,6 @@ import { FIREBASE_CONFIG } from './apiKey';
 import Rebase from 're-base';
 
 const ref = (process.env.NODE_ENV === 'production') ? 'session' : 'session';
-console.log(ref);
 export const firebaseApp = firebase.initializeApp(FIREBASE_CONFIG);
 export const firebaseDb = firebaseApp.database();
 export const sessionDb = firebaseDb.ref(ref);
