e031f4ed903d1d676ae5339414578a137fe08ea3
seiya kawamura <seiya.kawamura@gmail.com>
2017-02-02 00:42:28 +0900

    ロジック修正

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -47,7 +47,7 @@ class Header extends ReactBaseComponent {
     const { accessToken, name, photoURL } = app.currentUser;
     const isLogin = accessToken;
 
-    const usersNode = app.users.filter((u) => app.currentUser.uid !== u.uid).map((u, i) => {
+    const usersNode = app.users.filter((u) => app.currentUser.uid !== u.key).map((u, i) => {
       const temp = Object.values(u)[0];
       return (
         <img className="login-users__icons" key={i} src={temp.photoURL} alt={temp.name} />
