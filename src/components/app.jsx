9fb525d85417e74640e93911d1ebb0b078cd805c
Seiya Kawamura <seiya.kawamura@gmail.com>
2017-09-10 18:42:12 +0900

    remove redux

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,13 @@
 import React from 'react';
-import ReactBaseComponent from './reactBaseComponent';
-import { bindActionCreators } from 'redux';
-import { connect } from 'react-redux';
-import * as AppActions from '../actions/app';
-import { YoutubeApiUrl } from '../action_types/app';
-import { base, firebaseAuth } from '../config/firebaseApp';
+import { base } from '../config/firebaseApp.js';
 import 'whatwg-fetch';
 import classNames from 'classnames';
-import { DefaultVideo } from '../action_types/app';
 import ReactPlayer from 'react-player';
+import { YOUTUBE_API_KEY } from '../config/apiKey';
+import YouTubeNode from 'youtube-node';
+import { post, remove, push } from '../scripts/db';
 
 // Components
-import Header from './header';
 import SearchResult from './searchResult';
 import Comments from './comments';
 
@@ -21,127 +17,153 @@ import '../styles/normalize.scss';
 
 const youtubeUrl = (id) => `https://www.youtube.com/watch?v=${id}`;
 
-const channelsParams = (user) => (
-  `access_token=${user.accessToken}&part=contentDetails&mine=true`
-);
+// const userObj = ({ displayName, photoURL, uid, isAnonymous }, overrideState) => Object.assign(
+//   { displayName, photoURL, uid, isAnonymous }, overrideState
+// );
 
-const userObj = ({ displayName, photoURL, uid, isAnonymous }, overrideState) => Object.assign(
-  { displayName, photoURL, uid, isAnonymous }, overrideState
+const CommentType = { text: 'text', log: 'log', gif: 'gif' };
+const commentObj = (content, user, type, keyword) => (
+		Object.assign({ content, user, type, keyword })
 );
+const DefaultVideo = Object.assign({ id: '', title: '', thumbnail: { url: '' }, displayName: '' });
+const DefaultUser = Object.assign({ name: '', photoURL: '', accessToken: '', uid: '' });
 
-class App extends ReactBaseComponent {
+class App extends React.Component {
   constructor(props) {
     super(props);
-    this.bind('notification');
-    this.bind('getPlaylist', 'onUnload');
-				this.bind('onSeekMouseUp');
+    this.state = ({
+						currentUser: DefaultUser,
+						playingVideo: DefaultVideo,
+						que: [],
+						searchResult: [],
+						searchText: '',
+						searchedText: '',
+						isSearchActive: false,
+						isQueListActive: false,
+						isPlaylistActive: false,
+						isPlaying: true,
+						seeking: false,
+						duration: 0,
+						loaded: 0,
+						volume: 0.8,
+						played: 0,
+						startTime: 0,
+				});
+
+				this.onSeekMouseUp = this.onSeekMouseUp.bind(this);
+				this.onKeyPressForSearch = this.onKeyPressForSearch.bind(this);
+				this.goNext = this.goNext.bind(this);
+				this.progress = this.progress.bind(this);
   }
 
-		componentWillMount() {
-				firebaseAuth.getRedirectResult().then((result) => {
-						if (result.credential) {
-								const { accessToken } = result.credential;
-								const user = userObj(result.user, { accessToken, isHere: true });
-								this.props.actions.postUser(user.uid, user);
-								this.props.actions.setUser(user);
-						}
-				})
-				firebaseAuth.onAuthStateChanged((user) => {
-						if (user) {
-								if (user.isAnonymous) {
-										const temp = { displayName: 'User', photoURL: '../images/avatar.png', uid: user.uid, isAnonymous: user.isAnonymous };
-										const u = userObj(temp, { accessToken: '', isHere: true });
-										this.props.actions.postUser(u.uid, u);
-										this.props.actions.setUser(u);
-								} else {
-										base.listenTo(`users/${user.uid}`, { context: this, asArray: false, then(data) {
-												if(data) {
-														const u = userObj(data, { accessToken: data.accessToken, isHere: true });
-														this.props.actions.postUser(u.uid, u);
-														this.props.actions.setUser(u);
-														this.getPlaylist(u);
-												} else {
-														const u = userObj(user, { accessToken: '', isHere: true });
-														this.props.actions.postUser(u.uid, u);
-														this.props.actions.setUser(u);
-														this.getPlaylist(u);
-												}
-										}});
-								}
-						} else {
-								firebaseAuth.signInAnonymously();
-						}
-				})
-		}
-
 		componentDidMount() {
-				window.addEventListener("beforeunload", this.onUnload);
-				const { actions } = this.props;
 				base.listenTo('startTime', { context: this, asArray: false, then(startTime) {
-						actions.updatePlayed(startTime);
+						this.setState({ played: startTime });
 						this.player.seekTo(startTime);
 				}});
 				base.listenTo('playing', { context: this, asArray: false, then(playing) {
-						actions.updatePlaying(playing);
-				}});
-				base.listenTo('users', { context: this, asArray: true, then(users) {
-						actions.updateUsers(users);
+						this.setState({ isPlaying: playing });
 				}});
 				base.listenTo('playingVideo', { context: this, asArray: false, then(video) {
-						console.log(video);
-						actions.updatePlayingVideo(video);
+						const playingVideo = Object.keys(video).length === 0 ? DefaultVideo : video;
+						this.setState({ playingVideo });
 				}});
+				base.listenTo('que', { context: this, asArray: true, then(que) { this.setState({ que }) } });
 		}
 
-		componentWillUnmount(){
-				window.removeEventListener("beforeunload", this.onUnload);
+		onKeyPressForSearch(e) {
+				if (e.which !== 13) return false;
+				e.preventDefault();
+				this.search(this.state.searchText);
+				return true;
 		}
 
 		onSeekMouseUp(e) {
-				const played = parseFloat(e.target.value);
-				this.props.actions.seekUp(played);
-				this.player.seekTo(played);
+				const startTime = parseFloat(e.target.value);
+				this.setState({ seeking: false, startTime: startTime });
+				this.player.seekTo(startTime);
 		};
 
-  onUnload(e) {
-    const { currentUser } = this.props.app;
-    if (currentUser.isAnonymous) {
-      this.props.actions.removeUser(currentUser.uid);
-      firebaseAuth.signOut();
-    };
-    const u = Object.assign(currentUser, { isHere: false });
-    this.props.actions.postUser(u.uid, u);
-  };
+  search(searchText) {
+				const youTubeNode = new YouTubeNode();
+				youTubeNode.setKey(YOUTUBE_API_KEY);
+				youTubeNode.addParam('type', 'video');
+				youTubeNode.search(searchText, 50,
+						(error, result) => {
+								if (error) {
+										console.log(error);
+								} else {
+										const searchResult = result.items.map(item => ({
+												  id: item.id.videoId,
+														title: item.snippet.title,
+														thumbnailUrl: item.snippet.thumbnails.default.url,
+														type: 'video',
+										}));
+										this.setState({ searchResult, searchedText: searchText });
+								}
+						}
+				);
+		}
 
-  getPlaylist(user) {
-    fetch(`${YoutubeApiUrl}/channels?${channelsParams(user)}`)
-      .then((response) => { return response.json(); })
-      .then((json) => {
-        const base = json.items[0].contentDetails.relatedPlaylists;
-        const lists = Object.keys(base)
-                            .map((k) => ({ id: base[k], title: k, thumbnailUrl: ''}))
-        this.props.actions.setPlaylists(lists)
-      })
-      .catch((error) => console.log(error));
-  }
+		goNext() {
+  		const video = this.state.que[0];
+				if (video) {
+						post('playingVideo', video);
+						post('startTime', 0);
+						remove(`que/${video.key}`);
+						const comment = commentObj(`# ${video.title}`, video.user, CommentType.log, '');
+						push('comments', comment);
+				} else {
+						post('playingVideo', DefaultVideo);
+						post('startTime', 0);
+				}
+		}
 
-  notification(title, option) {
-    const notification = new Notification(
-      `${title} (${this.props.app.que.length + 1} remained)`,
-      { body: option.body, icon: option.icon, silent: true }
-    );
-    return notification;
-  }
+		progress({ played, loaded }) {
+				const playingStatus = (loaded) ? { played, loaded } : { played };
+				return !this.state.seeking && this.setState(playingStatus);
+		}
 
   render() {
-    const { app, searchResult, actions } = this.props;
-				const isPostPlayingVideo = app.playingVideo !== '';
-				const playingVideo = app.playingVideo || DefaultVideo;
-				const nextVideo = searchResult.que[0];
+				const isPostPlayingVideo = this.state.playingVideo !== '';
+				const playingVideo = this.state.playingVideo || DefaultVideo;
 
     return (
       <div className="contents">
-        <Header app={app} actions={actions} />
+								<header className="header-bar">
+										<div className="header-bar__left">
+												<div className="header-bar-prof">
+												</div>
+										</div>
+										<div
+												className={classNames('button-search-list', { 'is-search-active': this.state.isSearchActive })}
+												onClick={() => this.setState({ isSearchActive: !this.state.isSearchActive })}
+										>
+												<span />
+												<span />
+												<span />
+										</div>
+										<input
+												className={classNames('form-search', { 'is-search-active': this.state.isSearchActive })}
+												type="text"
+												placeholder="Search"
+												onChange={(e) => this.setState({ searchText: e.target.value })}
+												onFocus={() => {
+														this.setState({ isSearchActive: true, searchResult: [], isPlaylistActive: false });
+												}}
+												onKeyPress={this.onKeyPressForSearch}
+												value={this.state.searchText}
+										>
+										</input>
+										<div
+												className={classNames('button-que-list', { 'is-quelist-list': this.state.isQueListActive })}
+												onClick={() => this.setState({ isQueListActive: !this.state.isQueListActive})}
+										>
+												<span />
+												<span />
+												<span />
+										</div>
+								</header>
 								<div className="main-display">
 										<div className="display-youtube">
 												<ReactPlayer
@@ -150,45 +172,44 @@ class App extends ReactBaseComponent {
 														width={"100%"}
 														height={"100%"}
 														url={youtubeUrl(playingVideo.id)}
-														playing={app.playing}
-														volume={app.volume}
-														soundcloudConfig={app.soundcloudConfig}
-														vimeoConfig={app.vimeoConfig}
-														youtubeConfig={app.youtubeConfig}
-														fileConfig={app.fileConfig}
-														onReady={() => actions.play()}
-														onPlay={() => actions.play()}
-														onPause={() => actions.pause(app.played, app.duration)}
-														onEnded={() => actions.asyncPostPlayingVideo(nextVideo)}
-														onError={() => actions.asyncPostPlayingVideo(nextVideo)}
-														onProgress={actions.progress}
-														onDuration={(duration) => actions.changeValueWithKey('duration', duration)}
+														playing={this.state.isPlaying}
+														volume={this.state.volume}
+														youtubeConfig={this.state.youtubeConfig}
+														fileConfig={this.state.fileConfig}
+														onReady={() => console.log('Ready')}
+														onPlay={() => console.log('Play')}
+														onPause={() => console.log('Pause')}
+														onEnded={this.goNext}
+														onError={this.goNext}
+														onProgress={this.progress}
+														onDuration={(duration) => this.setState({ duration })}
 												/>
 										</div>
-										<Comments currentUser={app.currentUser} />
+										<Comments currentUser={this.state.currentUser} />
 										<SearchResult
-												currentUser={app.currentUser}
-												searchedText={app.searchedText}
-												isSearchActive={app.isSearchActive}
-												isQueListActive={app.isQueListActive}
-												searchResult={app.searchResult}
-												isNoPlayingVideo={app.playingVideo.title === ''}
+												que={this.state.que}
+												searchResult={this.state.searchResult}
+												currentUser={this.state.currentUser}
+												searchedText={this.state.searchedText}
+												isSearchActive={this.state.isSearchActive}
+												isQueListActive={this.state.isQueListActive}
+												isNoPlayingVideo={this.state.playingVideo.title === ''}
 										/>
 								</div>
 								<div className="footer-bar">
 										<div className="play-controll">
 												<button
 														className={classNames(
-																{ 'play-controll__pause': app.playing },
-																{ 'play-controll__play': !app.playing },
+																{ 'play-controll__pause': this.state.isPlaying },
+																{ 'play-controll__play': !this.state.isPlaying },
 														)}
-														onClick={() => actions.playPause(app.playing, app.duration)}
+														onClick={() => this.setState({ isPlaying: !this.state.isPlaying })}
 												>
 														&nbsp;
 												</button>
 												<button
 														className="play-controll__skip"
-														onClick={() => actions.asyncPostPlayingVideo(nextVideo)}
+														onClick={this.goNext}
 												>&nbsp;</button>
 										</div>
 										<div className="progress-box">
@@ -202,18 +223,18 @@ class App extends ReactBaseComponent {
 														<input
 																className="progress-bar__seek"
 																type="range" min={0} max={1} step="any"
-																value={app.played}
-																onMouseDown={actions.seekDown}
-																onChange={(e) => actions.changePlayed(parseFloat(e.target.value))}
+																value={this.state.played}
+																onMouseDown={() => this.setState({ seeking: false })}
+																onChange={(e) => this.setState({ played: parseFloat(e.target.value), seeking: false })}
 																onMouseUp={this.onSeekMouseUp}
 														/>
-														<div className="progress-bar__played" style={{ width: `${100 * app.played}%` }}>
+														<div className="progress-bar__played" style={{ width: `${100 * this.state.played}%` }}>
 														</div>
-														<div className="progress-bar__loaded" style={{ width: `${100 * app.loaded}%` }}>
+														<div className="progress-bar__loaded" style={{ width: `${100 * this.state.loaded}%` }}>
 														</div>
 												</div>
 												<div className="progress-box__status">
-														<p>{app.duration * app.played}</p>
+														<p>{this.state.duration * this.state.played}</p>
 												</div>
 										</div>
 
@@ -226,11 +247,11 @@ class App extends ReactBaseComponent {
 																min={0}
 																max={1}
 																step="any"
-																value={app.volume}
-																onChange={(e) => actions.changeVolume(e.target.value)}
+																value={this.state.volume}
+																onChange={(e) => this.setState({ volume: e.target.value}) }
 														/>
 												</div>
-												<p className="volume-box__ttl" onClick={() => actions.changeVolume(0)}>mute</p>
+												<p className="volume-box__ttl" onClick={() => this.setState({ volume: 0 })}>mute</p>
 										</div>
 								</div>
       </div>
@@ -238,16 +259,4 @@ class App extends ReactBaseComponent {
   }
 }
 
-App.propTypes = {
-  app: React.PropTypes.object,
-		searchResult: React.PropTypes.object,
-  actions: React.PropTypes.object,
-};
-
-const mapStateToProps = ({ app, searchResult }) => ({ app, searchResult });
-
-const mapDispatchToProps = (dispatch) => ({
-  actions: bindActionCreators(AppActions, dispatch),
-});
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default App;
