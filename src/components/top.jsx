28938bb25f4c3ff7171aba4c7b2f67fdbce88b70
yhirane <hirane04@gmail.com>
2017-09-12 13:37:30 +0900

    [add/modify]topコーディング開始

--- src/components/top.jsx
+++ src/components/top.jsx
@@ -1,5 +1,10 @@
 import React from 'react';
 
+// Styles
+import '../styles/base.scss';
+import '../styles/normalize.scss';
+import '../styles/top.scss';
+
 class Top extends React.Component {
  render() {
   return (
